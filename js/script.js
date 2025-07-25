d0d6485692923af5d7a018a159bc24ced6a251a3
a-yamamoto523 <yamamoto_a@cri-me.com>
2025-07-25 19:01:41 +0900

    ⚡️ Js_初歩の初歩

---
+++ js/script.js
@@ -0,0 +1,26 @@
+//文字　→ ””
+//数字　→ そのまま書く
+console.log("こんにちは");
+console.log(123);
+console.log("123");
+console.log(123 + 456);
+
+//要素を取得する
+document.getElementById('header');
+
+//変数を用意
+const kinnnikun = document.getElementById('header');
+//変数を出力
+console.log(kinnnikun);
+
+
+const apple = "りんご";
+console.log(apple);
+
+const mikan = 123 + 456;
+console.log(mikan);
+
+//クラスを一つだけ取得する
+const navigationItem =document.querySelectorAll('.navigationItem'); 
+console.log(navigationItem[4]);
+
